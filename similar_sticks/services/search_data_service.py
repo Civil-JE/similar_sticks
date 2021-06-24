0dd05cb3b0c6867fc90ef80b5eb3f201a519c364
Civil-JE <civil.dev.work@gmail.com>
2021-06-24 15:15:28 -0400

    distinctYears: Added a better way to get the distinct years from the Stick table for the dropdown. Also added more data for testing.

--- similar_sticks/services/search_data_service.py
+++ similar_sticks/services/search_data_service.py
@@ -1,10 +1,11 @@
-from similar_sticks.models import Stick, Curve, Flex, Make
+from similar_sticks.models import Stick, Curve, Flex, Make, db
 
 
 class SearchDataService:
 
     def get_default_dropdowns(self):
-        years = [{'id': year, 'name': year} for year in set([stick.year for stick in Stick.query.all()])]
+        distinct_years = db.session.query(Stick.year.distinct().label('year'))
+        years = [{'id': item.year, 'name': item.year} for item in distinct_years.all()]
         flexes = [flex.to_representation() for flex in Flex.query.all()]
         makes = [make.get_id_and_name() for make in Make.query.all()]
         curves = [curve.get_id_and_name() for curve in Curve.query.all()]
